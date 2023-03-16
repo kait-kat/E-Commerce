ebf3f56b3dd702ae1147e84f125ba88c6c3473ba
kait <kingkittykait@gmail.com>
2023-03-15 19:29:13 -0700

    defined all models and set up routes

--- models/index.js
+++ models/index.js
@@ -3,7 +3,26 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.belongsTo(Category,{
+  foreignKey: 'category_id'
+});
 
+Category.hasMany(Product,{
+  foreignKey: 'category_id'
+});
+
+Product.belongsToMany(Tag,{
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'product_id'
+
+});
+
+Tag.belongsToMany(Product,{
+  through: ProductTag,
+  as: 'product_tags',
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
     Product,
